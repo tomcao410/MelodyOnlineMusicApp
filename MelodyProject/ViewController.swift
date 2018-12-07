6782f10bc6868b4b8d78a76ed98da477790a9352
Tom <itom@toms-mbp.net.fpt>
2018-12-07 15:27:28 +0700

    Initial Commit

---
+++ MelodyProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MelodyProject
+//
+//  Created by Tom on 12/7/18.
+//  Copyright © 2018 Tom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
